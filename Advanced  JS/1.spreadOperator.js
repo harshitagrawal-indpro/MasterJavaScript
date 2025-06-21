1ff0b23f7366c85615c259e53c8a62977d2546bb
harshitagrawal-indpro <harshitagra8092@gmail.com>
2025-06-21 10:20:48 +0530

    not first commit

--- Advanced  JS/1.spreadOperator.js
+++ Advanced  JS/1.spreadOperator.js
@@ -6,3 +6,16 @@
 let arr  = [1,2,34,5,56];
 let arr2 = [...arr, 6, 7, 8]; // spread operator is used to convert an iterable to individualse elements
 console.log(arr2); // [1, 2, 34, 5, 56, 6, 7, 8]
+
+// Spread operator in functions used in two ways 1 ) in parameters amd 2) in arguments 
+
+// in parameters is called rest parameters
+
+function sum(a,b,...rest){
+    let sum = a + b;
+    for (i of rest){
+        sum +=i;
+    }
+    return sum ;
+}
+console.log(sum(1,2,3,4,5));
