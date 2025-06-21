deed24b9b1ccd7ef3dea19a32cfd3d09c9d66696
harshitagrawal-indpro <harshitagra8092@gmail.com>
2025-06-21 10:17:41 +0530

    not first commit

---
+++ Advanced  JS/1.spreadOperator.js
@@ -0,0 +1,8 @@
+// the spread operator is used to convert an iterable to individualse elements it is used to pack and inpack elements 
+// from an iterable or to an iterable 
+
+// eg 
+
+let arr  = [1,2,34,5,56];
+let arr2 = [...arr, 6, 7, 8]; // spread operator is used to convert an iterable to individualse elements
+console.log(arr2); // [1, 2, 34, 5, 56, 6, 7, 8]
